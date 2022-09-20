42fe0cdd4aebb62b0dd1d04167f843bd19f80e37
Patrickerson Veiga <patrickerson@gmail.com>
2022-09-20 00:14:20 -0300

    v3

--- src/model/EmailModel.py
+++ src/model/EmailModel.py
@@ -7,15 +7,11 @@ class EmailModel:
     
     def set_schema(
         self,
-        id=None,
-        date=None,
         emailFrom=None,
         emailTo=None,
         ):
-        self.id = id
-        self.date = date
         self.emailFrom = emailFrom
         self.emailTo = emailTo
         
     def export_dict(self):
-        return dict(id=self.id,date=self.date, emailFrom=self.emailFrom, emailTo=self.emailTo)
+        return dict(emailFrom=self.emailFrom, emailTo=self.emailTo)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,16 +7,26 @@ from filemanipulation.file import Files
 from model.EmailModel import EmailModel
 import json
 
+
+
+
+
 dirty_data = Dirs("dirty_data")
 preprocessing_data = Dirs("preprocessing_data")
-
-sub_dirs = dirty_data.list_subdirs()
-counter = 0
-wrong_counter=0
 path_wrong_data = "preprocessing_data/wrong_data"
 path_ready_data = "preprocessing_data/ready_data"
 wrongfiles = Dirs(path_wrong_data)
 readydata = Dirs(path_ready_data)
+
+
+def gen_dirs():
+    dirty_data.make_dir()
+    preprocessing_data.make_dir()
+    wrongfiles.make_dir()
+    readydata.make_dir()
+
+
+
 
 def clean_old_files():
     for file in wrongfiles.list_realpaths_subdir():
@@ -24,21 +34,18 @@ def clean_old_files():
     for file in readydata.list_realpaths_subdir():
         os.remove(file)
 
-subject_keyword="Subject"
-len_subject = len(subject_keyword)
+
+
+
 def create_file(content):
     schema = EmailModel()
     counter_to = 3
     to=content[counter_to].split(": ")[0]
     emailTo = content[counter_to].split(": ")[1]
     if not "To" in to:
-        for i in content:
-            if "X-To" in i:
-                if i.split(": ")[1] == "":
-                    return None 
+        return None
     if emailTo == "":
         counter_to+=2
-        print(content[counter_to])
         emailTo= content[counter_to]
     if emailTo.count(",")>1 and "@" in emailTo:
         replaced_list = emailTo.replace(" ", "").split(",")
@@ -53,15 +60,11 @@ def create_file(content):
                 replaced_list.append(c)
             counter_to+=1
         schema.set_schema(
-            id=content[0].split(": ")[1], 
-            date=content[1].split(": ")[1],
             emailFrom=content[2].split(": ")[1],
             emailTo=replaced_list
         )
     else:
         schema.set_schema(
-            id=content[0].split(": ")[1], 
-            date=content[1].split(": ")[1],
             emailFrom=content[2].split(": ")[1],
             emailTo=emailTo
             )
@@ -71,12 +74,20 @@ def create_file(content):
         gen_wrong_file = Files(f"{wrong_counter}.txt", data_workdir=path_wrong_data)
         var = "\n".join(reader)
         gen_wrong_file.write_file(f"schema: {dict_to_json}\nfilename: {var}\n")
+        print(to)
         return False
 
     gen_file = Files(f"{counter}.json", data_workdir=path_ready_data)
     gen_file.write_file(dict_to_json)
     return True
     
+
+gen_dirs()
+sub_dirs = dirty_data.list_subdirs()
+counter = 0
+wrong_counter=0
+subject_keyword="Subject"
+len_subject = len(subject_keyword)
 for i in sub_dirs:
     new_dir = Dirs(i)
     for j in new_dir.list_subdirs():
@@ -87,9 +98,9 @@ for i in sub_dirs:
             
             if f.valid():
                 reader = f.read_file().split("\n")
-                create = create_file(reader, f.filename)
+                create = create_file(reader)
                 if create == None:
-                    pass
+                    continue
                 elif create:
                     counter+=1
                 else:
@@ -98,7 +109,10 @@ for i in sub_dirs:
                 for sla in Dirs(cabo_criatividade).list_subdirs():
                     f = Files(sla)
                     reader = f.read_file().split("\n")
-                    if create_file(reader, f.filename):
+                    create = create_file(reader)
+                    if create == None:
+                        pass
+                    elif create:
                         counter+=1
                     else:
                         wrong_counter+=1
